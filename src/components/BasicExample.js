c24d9e354653bbd7624755574bc3213167325e5d
KRivera2394 <keanurivera2394@gmail.com>
2022-09-09 12:04:46 -0400

    First

---
+++ src/components/BasicExample.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import '../components/styles/header.css'
+
+
+
+function BasicExample({ currentPage, handlePageChange }) {
+  return (
+    <Navbar collapseOnSelect  bg="dark" expand="xl">
+      <Container className='wholeNav'>
+        <Navbar.Brand className='headerTitle'  href="#home">Slush-Trap</Navbar.Brand>
+        <Navbar.Toggle className='outHeader' aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse className='aboutHeader' id="basic-navbar-nav">
+          <Nav className='homeAndAboutContainer'>
+            <Nav.Link id='homeNav' href="#home" onClick={() => handlePageChange('Home')} className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'} >Home</Nav.Link>
+            <Nav.Link className='aboutHeader'  href="#about"
+          onClick={() => handlePageChange('About')}>About</Nav.Link>
+            
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default BasicExample;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div className='d-flex flex-column align-items-center mt-5'>
+      <h1 className='homeHeading mb-5'>SLUSH TRAP</h1>
+      <p className='homeText px-3'>
+      We're a mobile Slushy and snow cone food truck! 
+      We travel to different locations in the Tampa Bay area!
+      Check in with us to see where we will be next!
+      </p>
+
+      <iframe title='fbPost' src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fkeanu.rivera.33%2Fposts%2Fpfbid027wT52uwhSt3HmVGCJ5MwoNqXgeXZvUUqa6mPqNy3janpyoXEVw7pfzU8uXvxw2Bnl&show_text=true&width=500" width="500" height="166"  scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+
+   
+    </div>
+  );
+}
